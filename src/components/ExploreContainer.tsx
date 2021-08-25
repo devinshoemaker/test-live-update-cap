478f4598a5af3547daa2db243aae005e1811e1dc
Devin Shoemaker <devinshoe@gmail.com>
2021-08-25 11:23:39 -0500

    binary 1.1

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -8,7 +8,7 @@ const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
   return (
     <div className="container">
       <strong>{name}</strong>
-      <p>binary 1.0</p>
+      <p>binary 1.1</p>
     </div>
   );
 };
